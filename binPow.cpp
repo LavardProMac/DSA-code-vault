ae6174fc47476fd35e195c4f8ebf06e216d04cef
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2025-07-04 11:22:01 +0700

    Create binPow.cpp

---
+++ binPow.cpp
@@ -0,0 +1,14 @@
+const int M=1e9+7;
+
+inline ll Pow(ll a, ll b, ll r=1){
+    while(b){
+        if(b&1) r=r*a%M;
+        a=a*a%M, b>>=1;
+    }return r;
+}
+inline ll mul(ll a, ll b){
+    if(!b) return 0;
+    ll x=mul(a, b>>!);
+    return (b&1)? 
+        ((x<<1)+a)%M : 2*x%M;
+}
