faf9969fa2e9aa9d01f4c60db291e4591f019c5b
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2025-07-05 09:56:50 +0700

    Update ptich tsnt.cpp

--- ptich tsnt.cpp
+++ ptich tsnt.cpp
@@ -1,10 +1,10 @@
 inline void tsnt(ll n, int cnt=0){
     if(n%2==0){
         while(!(n&1)) n>>=1, ++cnt;
-        cout<<"2 "<<cnt<<'\n';
+        //cout<<"2 "<<cnt<<'\n';
     }for(int i=3; i*i<=n; i+=2){
         if(n%i) continue; cnt=0;
         while(n%i==0) n/=i, ++cnt;
-        cout<<i<<' '<<cnt<<'\n';
-    }if(n>1) cout<<n<<" 1\n";
+        //cout<<i<<' '<<cnt<<'\n';
+    }if(n>1) //cout<<n<<" 1\n";
 }
