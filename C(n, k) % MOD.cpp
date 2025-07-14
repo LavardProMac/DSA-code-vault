28dc45179f9626204e5833662b0b8c433156839b
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2025-07-14 18:52:35 +0700

    Update C(n, k) % MOD.cpp

--- C(n, k) % MOD.cpp
+++ C(n, k) % MOD.cpp
@@ -6,16 +6,16 @@ inline ll mu(ll b, ll e, ll r=1){
         if(e&1) r=r*b%M;
     return r;
 }
-inline void tsnt(ll x, ll d){
+inline void tsnt(ll x, const int& d){
     for(int i=2; i*i<=x; ++i)
         while(x%i==0) x/=i, p[i]+=d;
     if(x>1) p[x]+=d;
 }
-inline ll nCr(ll n, ll r){
+inline ll nCr(const int& n, const int& r){
     if(r<0 || r>n) return 0; ll res=1;
     for(int i=n-r+1; i<=n; ++i) tsnt(i,1);
     for(int i=1; i<=r; ++i) tsnt(i,-1);
     for(int i=2; i*i<=N; ++i) if(p[i]) 
-        res=res*mu(i,p[i])%M, p[i]=0;
+        res=res*mu(i, p[i])%M, p[i]=0;
     return res;
 }
