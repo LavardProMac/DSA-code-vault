b5e4a10396ef704fd0379c5e7cbd1777f3cb8a2d
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2025-07-23 11:19:56 +0700

    Update M.cpp

--- M.cpp
+++ M.cpp
@@ -7,24 +7,24 @@
 using namespace std;
 
 int main(){
-		ios::sync_with_stdio(0); cin.tie(0);
-		int t, m, n; cin>>t;
-		while(t--){ cin>>m>>n;
-			vector<vi> p(m, vi(n));
-			fo(i,0,m) fo(j,0,n) cin>>p[i][j];
-			int l=1, r=1e9, ans=0;
-			while(l<=r){	
-				int mid=(l+r)/2; vector<vi> c(n);
-				fo(i,0,m) fo(j,0,n) if(p[i][j]>=mid)
-				c[j].push_back(i); bool ok=1, kt=0;
-				fo(j,0,n) if(c[j].empty()) ok=0;
-				if(!ok){ r=mid-1; continue;}
-				unordered_map<int, int> mp;
-				fo(j,0,n) for(int i:c[j]) ++mp[i];
-				for(auto [f,s]:mp) if(s>=2){
-					kt=1; break;
-				}if(kt) ans=mid, l=mid+1;
-				else r=mid-1;
-			}cout<<ans<<'\n';
-		}
+	ios::sync_with_stdio(0); cin.tie(0);
+	int t, m, n; cin>>t;
+	while(t--){ cin>>m>>n;
+		vector<vi> p(m, vi(n));
+		fo(i,0,m) fo(j,0,n) cin>>p[i][j];
+		int l=1, r=1e9, ans=0;
+		while(l<=r){	
+			int mid=(l+r)/2; vector<vi> c(n);
+			fo(i,0,m) fo(j,0,n) if(p[i][j]>=mid)
+			c[j].push_back(i); bool ok=1, kt=0;
+			fo(j,0,n) if(c[j].empty()) ok=0;
+			if(!ok){ r=mid-1; continue;}
+			unordered_map<int, int> mp;
+			fo(j,0,n) for(int i:c[j]) ++mp[i];
+			for(auto [f,s]:mp) if(s>=2){
+				kt=1; break;
+			}if(kt) ans=mid, l=mid+1;
+			else r=mid-1;
+		}cout<<ans<<'\n';
+	}
 }
