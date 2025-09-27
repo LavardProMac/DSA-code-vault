66ec4d0d0c97f48e4a0ff69df2acc42c7bba3e15
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2025-09-27 08:33:39 +0700

    Create mss size <=k.cpp

---
+++ mss size <=k.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, k; cin>>n>>k;
+    deque<int> dq={0};
+    ll p[n+1], ans=-1e18; p[0]=0;
+    fo(i,1,n) cin>>p[i], p[i]+=p[i-1];
+    fo(i,1,n){
+        while(!dq.empty() && dq.front()<i-k)
+            dq.pop_front();
+        ans=max(ans, p[i]-p[dq.front()]);
+        while(!dq.empty() && p[dq.back()]>=p[i])
+            dq.pop_back();
+        dq.push_back(i);
+    }cout<<ans;
+}
