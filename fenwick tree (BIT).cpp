00070d6e6b2016f4b05695f6e6512ce68cbe3401
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2025-09-28 21:11:40 +0700

    Create fenwick tree (BIT).cpp

---
+++ fenwick tree (BIT).cpp
@@ -0,0 +1,14 @@
+using namespace std; const int N=1e6+1;
+ll a[N], bit[N];
+
+inline void upd(int i, ll v){
+    for(; i<=N; i+=i&-i) 
+        bit[i]=min(bit[i], v); //cập nhật min 
+        bit[i]+=v; //cập nhật phần tử
+}
+inline ll get(int i){ ll ans=9e18;
+    for(; i; i-=i&-i)
+        ans=min(ans, bit[i]); //lấy min đoạn
+        ans+=bit[i]; //lấy tổng đoạn
+    return ans;
+}
