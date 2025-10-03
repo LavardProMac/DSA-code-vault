b55ae05144cf967ff8748e1fb81693199910e1bd
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2025-10-03 17:09:18 +0700

    Update sanguoc.cpp

--- sanguoc.cpp
+++ sanguoc.cpp
@@ -2,14 +2,14 @@ using namespace std; const int N=1e6+1;
 int spf[N], a[N]; //spf : smallest prime factor
 //O(N log N)
 inline void sanguoc(){
-    for(int i=1; i<N; ++i)
-        for(int j=i; j<N; j+=i) ++a[j];
+    fo(i,1,N) for(int j=i; j<N; j+=i) ++a[j];
 }
 //O(N log log N)
 inline void sangSpf(){ spf[1]=1;
-    for(int i=2; i<N; ++i) if(!spf[i]){
+    for(int i=2; i<N; i+=2) spf[i]=2;
+    for(int i=3; i<N; i+=2) if(!spf[i]){
         spf[i]=i;
-        for(int j=i*i; j<N; j+=i)
+        for(int j=i*i; j<N; j+=i<<1)
             if(!spf[j]) spf[j]=i;
     }
 }
