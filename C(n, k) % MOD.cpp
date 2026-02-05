1a0c55a4376e242587ebf6d431950772f047a835
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-05 23:25:16 +0700

    Update C(n, k) % MOD.cpp

--- C(n, k) % MOD.cpp
+++ C(n, k) % MOD.cpp
@@ -1,21 +1,26 @@
-using namespace std; const int N=1e6+1;
-ll p[N]; const int M=2024;
+const int N=1e6, M=2024;
+ll p[N+1];
 
-inline ll mu(ll b, ll e, ll r=1){
-    for(; e; e>>=1, b=b*b%M)
-        if(e&1) r=r*b%M;
-    return r;
+ll Pow(ll a, ll b){
+    ll res=1;
+    for(; b; b>>=1, a=a*a%M)
+        if(b&1) res=res*a%M;
+    return res;
 }
-inline void tsnt(ll x, const int& d){
-    for(int i=2; i*i<=x; ++i)
+
+void tsnt(ll x, const int& d){
+    while(!(x&1)) x>>=1, p[2]+=d;
+    for(int i=3; i*i<=x; i+=2)
         while(x%i==0) x/=i, p[i]+=d;
     if(x>1) p[x]+=d;
 }
-inline ll nCr(const int& n, const int& r){
-    if(r<0 || r>n) return 0; ll res=1;
-    for(int i=n-r+1; i<=n; ++i) tsnt(i,1);
-    for(int i=1; i<=r; ++i) tsnt(i,-1);
-    for(int i=2; i*i<=N; ++i) if(p[i]) 
-        res=res*mu(i, p[i])%M, p[i]=0;
+
+ll nCr(const int& n, const int& r){
+    if(r<0 || r>n) return 0;
+    ll res=1;
+    fo(i,n-r+1,n) tsnt(i, 1);
+    fo(i,1,r) tsnt(i, -1);
+    fo(i,2,1000) if(p[i]) 
+        res=res*Pow(i, p[i])%M, p[i]=0;
     return res;
 }
