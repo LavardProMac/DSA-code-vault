2e7079c9ecde4c590678e7b31ffe6be586876605
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-05 23:15:28 +0700

    Update mss size <=k.cpp

--- mss size <=k.cpp
+++ mss size <=k.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
+    
     int n, k; cin>>n>>k;
-    deque<int> dq={0};
     ll p[n+1], ans=-1e18; p[0]=0;
+    deque<int> dq={0};
+    
     fo(i,1,n) cin>>p[i], p[i]+=p[i-1];
+    
     fo(i,1,n){
         while(!dq.empty() && dq.front()<i-k)
             dq.pop_front();
+        
         ans=max(ans, p[i]-p[dq.front()]);
+        
         while(!dq.empty() && p[dq.back()]>=p[i])
             dq.pop_back();
+        
         dq.push_back(i);
-    }cout<<ans;
+    }
+    cout<<ans;
 }
