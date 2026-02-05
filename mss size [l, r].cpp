7a570a0c690f5f0be8f1bd4d53ed680cac525eac
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-05 23:17:05 +0700

    Update mss size [l, r].cpp

--- mss size [l, r].cpp
+++ mss size [l, r].cpp
@@ -6,16 +6,23 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int n, l, r; cin>>n>>l>>r;
-    ll a[n+1], p[n+1]; p[0]=0;
-    fo(i,1,n) cin>>a[i], p[i]=p[i-1]+a[i];
-    deque<ll> dq; ll ans=-1e18;
+    
+    ll p[n+1], ans=-1e18; p[0]=0;
+    deque<ll> dq;
+    
+    fo(i,1,n) cin>>p[i], p[i]+=p[i-1];
+    
     fo(i,l,n){
         int j=i-l;
+        
         while(!dq.empty() && p[dq.back()]>=p[j])
             dq.pop_back();
+        
         dq.push_back(j);
+        
         while(!dq.empty() && dq.front()<i-r)
             dq.pop_front();
+        
         ans=max(ans, p[i]-p[dq.front()]);
     }
     cout<<ans;
