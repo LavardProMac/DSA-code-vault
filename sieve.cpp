ad9c1f8a3b54afccfe0cdc78898d2c697e55a674
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-07 20:42:33 +0700

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -20,12 +20,14 @@ void sieve(){
 }
 
 //linear sieve O(N)
-int a[N+1], p[N+1];
+int a[N+1], v[N+1], pc;
+bitset<N+1> p;
 
 void sieve(){
     fo(i,2,N){
-        if(!a[i]) a[i]=i, p[++pc]=i;
-        for(int j=1; j<=pc && p[j]<=a[i] && i*p[j]<=N; ++j)
-            a[i*p[j]]=p[j];
+        if(!a[i]) a[i]=i, v[++pc]=i, p[i]=1;
+        for(int j=1; j<=pc && v[j]<=a[i] && i*v[j]<=N; ++j)
+            a[i*p[j]]=v[j];
     }
 }
+
