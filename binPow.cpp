b329ee4d1e8e6eef4cce647f3a519f6052e28f4a
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-14 16:54:52 +0700

    Update binPow.cpp

--- binPow.cpp
+++ binPow.cpp
@@ -1,13 +1,19 @@
 const int M=1e9+7;
 
-ll Pow(ll a, ll b, ll r=1){
+inline ll Pow(ll a, ll b){
+    ll res=1;
     while(b){
-        if(b&1) r=r*a%M;
+        if(b&1) res=res*a%M;
         a=a*a%M, b>>=1;
-    }return r;
+    }
+    return res;
 }
-ll mul(ll a, ll b){
-    if(!b) return 0;
-    ll x=mul(a, b>>1)<<1;
-    return (x+(b&1)*a)%M;
+
+inline ll Mul(ll a, ll b){
+    ll res=0;
+    while(b){
+        if(b&1) res=(res+a)%M;
+        a=(a+a)%M; b>>=1;
+    }
+    return res;
 }
