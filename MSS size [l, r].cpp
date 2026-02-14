2598bc1c0ff61b3d5e16d987c487d1e31942673c
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-14 16:52:07 +0700

    Update MSS size [l, r].cpp

--- MSS size [l, r].cpp
+++ MSS size [l, r].cpp
@@ -1,27 +1,26 @@
 #include<bits/stdc++.h>
 #define ll long long
+#define Max(a,b) ({int v=b; a=a>v? a:v;})
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
+    
     int n, l, r; cin>>n>>l>>r;
-    
     ll p[n+1], ans=-1e18; p[0]=0;
-    deque<int> dq;
     
+    deque<int> dq;
     fo(i,1,n) cin>>p[i], p[i]+=p[i-1];
     
     fo(i,l,n){
         while(!dq.empty() && p[dq.back()]>=p[i-l])
             dq.pop_back();
-        
         dq.push_back(j);
         
         while(!dq.empty() && dq.front()<i-r)
             dq.pop_front();
-        
-        ans=max(ans, p[i]-p[dq.front()]);
+        Max(ans, p[i]-p[dq.front()]);
     }
     cout<<ans;
 }
