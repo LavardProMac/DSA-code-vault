03eae037c5d32e551965bd9e20bc5f3b8a293815
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-14 16:34:51 +0700

    Update sparse table.cpp

--- sparse table.cpp
+++ sparse table.cpp
@@ -2,30 +2,25 @@
 #define ll long long
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
-const int N=1e6+5, LOG=20;
-ll a[N], st[LOG][N];
-int lg[N], n, q;
+
+const int N=1e6+1, LOG=20;
+int n, lg[N]; ll a[N], st[LOG][N];
 
 void tienxuly(){
-    lg[1]=0;
-    fo(i,2,n) lg[i]=lg[i/2]+1;
-    fo(i,1,n) st[0][i]=a[i];
-    for(int k=1; (1<<k)<=n; ++k)
-        fo(i,1,n-(1<<k)+1)
-            st[k][i]=min(st[k-1][i], 
-            st[k-1][i+(1<<(k-1))]);
+    lg[1]=0; st[0][1]=a[1];
+    fo(i,2,n) lg[i]=lg[i>>1]+1, st[0][i]=a[i];
+    for(int k=1, t=2; t<=n; ++k, t<<=1) fo(i,1,n-t+1)
+        st[k][i]=min(st[k-1][i], st[k-1][i+(t>>1)]);
 }
-inline ll qry(int l, int r){
-    int j=lg[r-l+1];
-    return min(st[j][l],
-           st[j][r-(1<<j)+1]);
+
+inline ll qry(const int& l, const int& r){
+    const int j=lg[r-l+1];
+    return min(st[j][l], st[j][r-(1<<j)+1]);
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    cin>>n>>q;
-    fo(i,1,n) cin>>a[i];
-    tienxuly(); int l, r;
-    while(q--) cin>>l>>r,
-        cout<<qry(l, r)<<'\n';
+    int q, l, r; cin>>n>>q;
+    fo(i,1,n) cin>>a[i]; tienxuly();
+    while(q--) cin>>l>>r, cout<<qry(l, r)<<'\n';
 }
