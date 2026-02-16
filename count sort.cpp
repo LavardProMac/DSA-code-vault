34e056282d73e3aaf9e67e7e5743ba3662a88083
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-16 15:26:59 +0700

    Update count sort.cpp

--- count sort.cpp
+++ count sort.cpp
@@ -1,10 +1,7 @@
-#include<bits/stdc++.h>
-#define fo(i,j,n) for(int i=j; i<=n; ++i)
-using namespace std; const int N=1e6+1;
-int n, a[N], c[N+1];
+const int N=1e6;
+int n, j, a[N+1], c[N+1];
 
-inline void cntSort(){
-    memset(c, 0, sizeof(c));
-    fo(i,1,n) ++c[a[i]]; int j=0;
+void count_sort(){
+    fo(i,1,n) ++c[a[i]];
     fo(i,1,N) while(c[v]--) a[++j]=v;
 }
