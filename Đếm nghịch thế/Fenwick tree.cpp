e05c0220e6553191c0023ab6a290bfa8f6799bf7
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-02-16 15:54:50 +0700

    Update Fenwick tree.cpp

--- Đếm nghịch thế/Fenwick tree.cpp
+++ Đếm nghịch thế/Fenwick tree.cpp
@@ -1,4 +1,4 @@
-// Sol 2: Fenwick tree (BIT) O(n log N)
+// Sol 2: Fenwick tree O(n log N)
 // Tối ưu khi ai ≤ 1e6
 
 #include<bits/stdc++.h>
@@ -6,19 +6,19 @@
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
 #define fd(i,j,n) for(int i=j; i>=n; --i)
 using namespace std;
-const int N=1e6; int t[N+1];
+const int N=1e6; int bit[N+1];
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     
-    int n; cin>>n; int a[n+1]; ll cnt=0;
+    int n; ll cnt=0; cin>>n; int a[n+1];
     fo(i,1,n) cin>>a[i];
     
     fd(i,n,1){
         int s=0, k=a[i];
-        for(int j=k-1; j; j-=j&-j) s+=t[j];
+        for(int j=k-1; j; j-=j&-j) s+=bit[j];
         cnt+=s;
-        for(int j=k; j<=N; j+=j&-j) ++t[j];
+        for(int j=k; j<=N; j+=j&-j) ++bit[j];
     }
     cout<<cnt;
 }
