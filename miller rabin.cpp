682132602fa09a6df66a3a0e99dca7427571798d
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-03-01 20:27:13 +0700

    Update miller rabin.cpp

--- miller rabin.cpp
+++ miller rabin.cpp
@@ -1,17 +1,25 @@
-inline int binPow(int a, int k, const ll& n){ int r=1;
-    for(a%=n; k; k>>=1, a=(1LL*a*a)%n) if(k&1) r=(1LL*r*a)%n;
-    return r;
+ll Pow(ll a, int k, ll M){
+    ll r=1; a%=n;
+    for(; k; k>>=1, a=a*a%M)
+        if(k&1) res=res*a%M;
+    return res;
 }
-inline bool test(const int& a, const ll& n, const int& k, const int& m){
-    int x=binPow(a,m,n); if(x==1 || x==n-1) return true;
-    for(int i=1; i<k; ++i) if((x=1LL*x*x%n)==n-1) return true;
-    return false;
+
+bool test(int a, ll n, int k, int m){
+    ll x=Pow(a, m, n--);
+    if(x==1 || x==n) return 0;
+    
+    fo(i,0,k) if((x=x*x%n)==n) return 1;
+    return 0;
 }
-inline bool isP(const ll& n){
-    if(n<2) return false; if(n<4) return true;
-    if(!(n&1 && n%3)) return false;
-    int k=0, m=n-1; while(!(m&1)) m>>=1, ++k;
-    if(!test(2,n,k,m)) return false;
-    if(!test(3,n,k,m)) return false;
-    return true;
+
+bool isP(ll n){
+    if(n<2) return 0; if(n<4) return 1;
+    if(!(n&1 && n%3)) return 0;
+    
+    int k=0, m=n-1;
+    while(!(m&1)) m>>=1, ++k;
+    
+    if(kt(2,n,k,m)||kt(3,n,k,m)) return 0;
+    return 1;
 }
