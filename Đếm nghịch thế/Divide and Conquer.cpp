41fbf0124452f6f8dc411392dff5dfeac1d485ca
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-03-03 21:54:30 +0700

    Update Divide and Conquer.cpp

--- Đếm nghịch thế/Divide and Conquer.cpp
+++ Đếm nghịch thế/Divide and Conquer.cpp
@@ -5,35 +5,29 @@
 #define ll long long
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
-
-const int N=1e6+1;
-int a[N], kq[N];
+const int N=1e6+1; int a[N], t[N];
 
 // đếm nghịch thế = merge sort
-ll cnt(int l, int m, int r){
-    int i=l, j=m+1, k=l; ll d=0; 
+ll MS(int l, int r){
+    if(l>=r) return 0;
+    
+    int m=l+r>>1, i=l, j=m+1, k=l;
+    ll cnt=MS(l, m)+MS(m+1, r);
     
     while(i<=m && j<=r)
-        if(a[i]<=a[j]) kq[k++]=a[i++];
-        else kq[k++]=a[j++], d+=m-i+1;
+        if(a[i]<=a[j]) t[k++]=a[i++];
+        else t[k++]=a[j++], cnt+=m-i+1;
     
-    while(i<=m) kq[k++]=a[i++];
-    while(j<=r) kq[k++]=a[j++];
+    while(i<=m) t[k++]=a[i++];
+    while(j<=r) t[k++]=a[j++];
     
-    fo(i,l,r) a[i]=kq[i];
-    return d;
-}
-
-// đệ quy đếm toàn bộ
-ll dq(int l, int r){
-    if(l>=r) return 0; int m=l+r>>1;
-    return dq(l,m)+dq(m+1,r)+cnt(l,m,r);
+    fo(i,l,r) a[i]=t[i];
+    return cnt;
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin>>n;
-    fo(i,0,n) cin>>a[i];
-    
-    cout<<dq(0, n-1);
+    fo(i,1,n) cin>>a[i];
+    cout<<MS(1, n);
 }
