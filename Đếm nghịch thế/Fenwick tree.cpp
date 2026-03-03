44e149321585094ea15171eef7e560218f1cd59f
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-03-03 21:21:57 +0700

    Update Fenwick tree.cpp

--- Đếm nghịch thế/Fenwick tree.cpp
+++ Đếm nghịch thế/Fenwick tree.cpp
@@ -4,21 +4,29 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
-#define fd(i,j,n) for(int i=j; i>=n; --i)
 using namespace std;
-const int N=1e6; int bit[N+1];
+
+const int N=1e6; int n, bit[N+1];
+
+void upd(int i){
+    for(; i<=N; i+=i&-i) ++bit[i];
+}
+
+int sum(int i, int s=0){
+    for(; i; i-=i&-i) s+=bit[i];
+    return s;
+}
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     
-    int n; ll cnt=0; cin>>n; int a[n+1];
+    cin>>n; int a[n+1]; ll cnt=0;
     fo(i,1,n) cin>>a[i];
     
-    fd(i,n,1){
-        int s=0, k=a[i];
-        for(int j=k-1; j; j-=j&-j) s+=bit[j];
-        cnt+=s;
-        for(int j=k; j<=N; j+=j&-j) ++bit[j];
-    }
+    for(int i=n; i; --i)
+        cnt+=sum(a[i]-1), upd(a[i]);     
+    // tính tổng dãy bit[1, a[i]) và tăng bit[a[i]] lên 1 đơn vị
+    // vì duyệt ngược mảng a nên trở thành đếm nghịch thế
+    
     cout<<cnt;
 }
