670a0fd25b6151a8940ad25c23fdd9e387a7994f
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-04-01 23:03:51 +0700

    Update point sum.cpp

--- Segment tree/point sum.cpp
+++ Segment tree/point sum.cpp
@@ -29,7 +29,6 @@ void upd(int id,int l,int r,int pos,ll val){
 ll get(int id,int l,int r,int u,int v){
     if(r<u || l>v) return 0;
     if(u<=l && r<=v) return st[id];
-
     int m=l+r>>1, k=id<<1;
     
     return get(k,l,m,u,v)+
@@ -42,7 +41,6 @@ int main(){
 
     while(q--){
         cin>>t>>l>>r;
-        
         if(t==1) upd(1,1,n,l,r);
         else cout<<get(1,1,n,l,r)<<'\n';
     }
