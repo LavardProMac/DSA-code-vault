f83553ffbcfadcde9577da7469b562b8883ed7ee
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-04-01 20:03:22 +0700

    Update lazy propagation.cpp

--- lazy propagation.cpp
+++ lazy propagation.cpp
@@ -5,9 +5,19 @@
 #define ll long long
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
-ll st[400001], lz[400001];
+
+const int N=1e5+1;
+int a[N]; ll st[N<<2], lz[N<<2];
 
 void build(int id, int l, int r){
+    if(l==r) {st[id]=a[l]; return;}
+    int m=l+r>>1, k=id<<1;
+
+    build(k,l,m); build(k|1,m+1,r);
+    st[id]=st[k]+st[k|1];
+}
+
+void push(int id, int l, int r){
     if(!lz[id]) return;
     int m=l+r>>1, k=id<<1;
 
@@ -26,7 +36,7 @@ void upd(int id,int l,int r,int u,int v,ll k){
         st[id]+=k*(r-l+1);
         lz[id]+=k; return;
     }
-    build(id, l, r);
+    push(id, l, r);
     int m=l+r>>1, t=id<<1;
 
     upd(t, l, m, u, v, k);
@@ -35,11 +45,11 @@ void upd(int id,int l,int r,int u,int v,ll k){
     st[id]=st[t]+st[t|1];
 }
 
-ll get(int id,int l,int r,int u,int v){
+ll get(int id, int l, int r, int u, int v){
     if(r<u || l>v) return 0;
     if(u<=l && r<=v) return st[id];
     
-    build(id, l, r); int m=l+r>>1;
+    push(id, l, r); int m=l+r>>1;
     
     return get(id<<1,l,m,u,v)+
            get(id<<1|1,m+1,r,u,v);
