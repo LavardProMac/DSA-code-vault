752c41fe0032371291d32aaaea6fc9509f857fa3
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-04-01 20:35:41 +0700

    Update and rename Lazy sum.cpp to lazy sum.cpp

---
+++ Segment tree/lazy sum.cpp
@@ -0,0 +1,68 @@
+// Name: Range updates and sums
+// Source: lqdoj.edu.vn/problem/sumq
+
+#include<bits/stdc++.h>
+#define ll long long
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
+using namespace std;
+
+const int N=1e5+1;
+int a[N]; ll st[N<<2], lz[N<<2];
+
+void build(int id, int l, int r){
+    if(l==r) {st[id]=a[l]; return;}
+    int m=l+r>>1, k=id<<1;
+
+    build(k,l,m); build(k|1,m+1,r);
+    st[id]=st[k]+st[k|1];
+}
+
+void push(int id, int l, int r){
+    if(!lz[id]) return;
+    int m=l+r>>1, k=id<<1;
+
+    st[k]+=lz[id]*(m-l+1);
+    st[k|1]+=lz[id]*(r-m);
+
+    lz[k]+=lz[id];
+    lz[k|1]+=lz[id];
+    lz[id]=0;
+}
+
+void upd(int id,int l,int r,int u,int v,ll k){
+    if(r<u || l>v) return;
+
+    if(u<=l && r<=v){
+        st[id]+=k*(r-l+1);
+        lz[id]+=k; return;
+    }
+    push(id, l, r);
+    int m=l+r>>1, t=id<<1;
+
+    upd(t, l, m, u, v, k);
+    upd(t|1, m+1, r, u, v, k);
+
+    st[id]=st[t]+st[t|1];
+}
+
+ll get(int id, int l, int r, int u, int v){
+    if(r<u || l>v) return 0;
+    if(u<=l && r<=v) return st[id];
+    
+    push(id, l, r); int m=l+r>>1;
+    
+    return get(id<<1,l,m,u,v)+
+           get(id<<1|1,m+1,r,u,v);
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, q, t, l, r; ll k; cin>>n>>q;
+
+    while(q--){
+        cin>>t>>l>>r;
+        
+        if(!t) cin>>k, upd(1,1,n,l,r,k);
+        else cout<<get(1,1,n,l,r)<<'\n';
+    }
+}
