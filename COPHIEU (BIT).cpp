1e30145119f9528fc2ab3250000c1ee4e70ef768
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-04-25 21:47:32 +0700

    Update COPHIEU (BIT).cpp

--- COPHIEU (BIT).cpp
+++ COPHIEU (BIT).cpp
@@ -7,14 +7,12 @@
 using namespace std;
 const int N=1e5+5; int bit[N];
 
-void upd(int i, int v){
-    for(; i<N; i+=i&-i)
-        bit[i]=min(bit[i], v);
+void upd(int i, const int& v){
+    for(; i<N; i+=i&-i) bit[i]=min(bit[i], v);
 }
 
 int get(int i, int m=1e9){
-    for(; i; i-=i&-i)
-        m=min(m, bit[i]);
+    for(; i; i-=i&-i) m=min(m, bit[i]);
     return m;
 }
 
@@ -24,7 +22,7 @@ int main(){
     freopen("COPHIEU.OUT", "w", stdout);
     
     int n, x; cin>>n;
-    fill(bit, bit+N, 1e9);
+    memset(bit, 0x3f, sizeof bit);
 
     fo(i,1,n) cin>>x,
         cout<<max(0, i-get(x-1))<<' ',
