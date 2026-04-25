bc1b9e237fb74a4c8b6718673801f1b5adc300ae
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-04-25 15:07:30 +0700

    Update miller rabin.cpp

--- miller rabin.cpp
+++ miller rabin.cpp
@@ -1,25 +1,35 @@
-ll Pow(ll a, int k, ll M){
-    ll res=1; a%=M;
-    for(; k; k>>=1, a=a*a%M)
-        if(k&1) res=res*a%M;
-    return res;
+inline ll mul(ll a, ll b, ll m){
+    return (__int128)a*b%m;
 }
 
-bool kt(int a, ll n, int k, int m){
-    ll x=Pow(a, m, n--);
-    if(x==1 || x==n) return 0;
-    
-    fo(i,0,k) if((x=x*x%n)==n) return 1;
-    return 0;
+ll pw(ll a, ll d, ll m){
+    ll r=1;
+    for(; d; d>>=1, a=mul(a, a, m))
+        if(d&1) r=mul(r, a, m);
+    return r;
 }
 
 bool isP(ll n){
-    if(n<2) return 0; if(n<4) return 1;
-    if(!(n&1 && n%3)) return 0;
+    if(n<2) return 0;
     
-    int k=0, m=n-1;
-    while(!(m&1)) m>>=1, ++k;
-    
-    if(kt(2,n,k,m)||kt(3,n,k,m)) return 0;
+    for(int p:{2,3,5,7,11,13,17,19,23,29})
+        if(n%p==0) return n==p;
+
+    ll d=n-1, s=0;
+    while(!(d&1)) d>>=1, ++s;
+
+    for(int a:{2,325,9375,28178,450775,9780504,1795265022}){
+        if(a%n==0) continue;
+        ll x=pw(a, d, n);
+        
+        if(x==1 || x==n-1) continue;
+        bool ok=0;
+        
+        fo(i,1,s-1){
+            x=mul(x, x, n);
+            if(x==n-1) {ok=1; break;}
+        }
+        if(!ok) return 0;
+    }
     return 1;
 }
