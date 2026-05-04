4a993bd7cb05fc0508e33257c14e77edf790d966
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-04 15:55:35 +0700

    Update lazy sum.cpp

--- Segment tree/lazy sum.cpp
+++ Segment tree/lazy sum.cpp
@@ -21,26 +21,18 @@ void push(int id, int l, int r){
     if(!lz[id]) return;
     int m=l+r>>1, k=id<<1;
 
-    st[k]+=lz[id]*(m-l+1);
-    st[k|1]+=lz[id]*(r-m);
-
-    lz[k]+=lz[id];
-    lz[k|1]+=lz[id];
-    lz[id]=0;
+    st[k]+=lz[id]*(m-l+1); st[k|1]+=lz[id]*(r-m);
+    lz[k]+=lz[id]; lz[k|1]+=lz[id]; lz[id]=0;
 }
 
-void upd(int id,int l,int r,int u,int v,ll k){
+void upd(int id, int l, int r, int u, int v, ll k){
     if(r<u || l>v) return;
 
     if(u<=l && r<=v){
-        st[id]+=k*(r-l+1);
-        lz[id]+=k; return;
+        st[id]+=k*(r-l+1); lz[id]+=k; return;
     }
-    push(id, l, r);
-    int m=l+r>>1, t=id<<1;
-
-    upd(t, l, m, u, v, k);
-    upd(t|1, m+1, r, u, v, k);
+    int m=l+r>>1, t=id<<1; push(id, l, r);
+    upd(t,l,m,u,v,k); upd(t|1,m+1,r,u,v,k);
 
     st[id]=st[t]+st[t|1];
 }
@@ -48,18 +40,17 @@ void upd(int id,int l,int r,int u,int v,ll k){
 ll get(int id, int l, int r, int u, int v){
     if(r<u || l>v) return 0;
     if(u<=l && r<=v) return st[id];
-    push(id, l, r); int m=l+r>>1;
     
-    return get(id<<1,l,m,u,v)+
-           get(id<<1|1,m+1,r,u,v);
+    int m=l+r>>1, k=id<<1; push(id, l, r);
+    return get(k,l,m,u,v)+get(k|1,m+1,r,u,v);
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int n, q, t, l, r; ll k; cin>>n>>q;
+    int n, q; cin>>n>>q;
 
     while(q--){
-        cin>>t>>l>>r;
+        int t, l, r; ll k; cin>>t>>l>>r;
         if(!t) cin>>k, upd(1,1,n,l,r,k);
         else cout<<get(1,1,n,l,r)<<'\n';
     }
