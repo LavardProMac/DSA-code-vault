31b73e2dd43baf712f16ce9d75258dbf009c69c8
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-04 16:28:43 +0700

    Update prefix sum 2d.cpp

--- prefix sum 2d.cpp
+++ prefix sum 2d.cpp
@@ -2,27 +2,15 @@
 #define ll long long
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
-
-const int N=1001;
-int a[N][N]; ll p[N][N];
+ll p[1001][1001];
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int m, n; cin>>m>>n;
+    int n, q, x, x1, y1, x2, y2; cin>>n>>q;
     
-    fo(i,1,m) fo(j,1,n)
-        cin>>a[i][j], p[i][j]=
-        
-        p[i-1][j]+p[i][j-1]-
-        p[i-1][j-1]+a[i][j];
-        
-    int t, x1, y1, x2, y2; cin>>t;
+    fo(i,1,n) fo(j,1,n) cin>>x,
+        p[i][j]=p[i-1][j]+p[i][j-1]-p[i-1][j-1]+x;
     
-    while(t--)
-        cin>>x1>>y1>>x2>>y2,
-        
-        cout<<
-            p[x2][y2]+p[x1-1][y1-1]-
-            p[x2][y1-1]-p[x1-1][y2];
-        <<'\n';
+    while(q--) cin>>x1>>y1>>x2>>y2,
+        cout<<p[x2][y2]-p[x2][y1-1]-p[x1-1][y2]+p[x1-1][y1-1]<<'\n';
 }
