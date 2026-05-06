4cbd43b81455e2a9423870a5feaf96c8fed97486
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-06 22:13:42 +0700

    Update prep palin.cpp

--- prep palin.cpp
+++ prep palin.cpp
@@ -1,24 +1,27 @@
-// palindrome preprocess O(N^2)
+// Palindrome preprocess O(N^2)
 
 #include<bits/stdc++.h>
 #define ll long long
-#define fo(i,j,n) for(int i=j; i<n; ++i)
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
-string s; bool p[2001][2001];
+string s; bool p[10001][10001];
 
 void prep(){
-    int n=s.size();
-    fo(i,0,n) p[i][i]=1;
+    int n=s.size(); s=' '+s;
     
-    fo(i,0,n-1) if(s[i]==s[i+1])
-        p[i][i+1]=1;
-    
-    for(int l=n-1; l>=0; --l) fo(r,l+2,n)
-        if(s[l]==s[r] && p[l+1][r-1])
-            p[l][r]=1;
+    fo(i,1,n){
+        p[i][i]=1;
+        if(s[i]==s[i-1]) p[i-1][i]=1;
+    }
+    for(int l=n; l; --l) fo(r,l+2,n)
+        if(s[l]==s[r] && p[l+1][r-1]) p[l][r]=1;
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     cin>>s; prep();
+    int q, l, r; cin>>q;
+    
+    while(q--) cin>>l>>r,
+        cout<<(p[l][r]? "YES\n":"NO\n");
 }
