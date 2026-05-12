4b039b5a95dc4389f8846744c9aae73cb034f011
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-12 20:37:49 +0700

    Update prime_check.cpp

--- prime_check.cpp
+++ prime_check.cpp
@@ -1,7 +1,9 @@
-inline bool isP(const ll& n){
+bool isP(const ll& n){
     if(n<2) return 0; if(n<4) return 1;
     if(!(n&1 && n%3)) return false;
+    
     for(int i=5, s=sqrt(n); i<=s; i+=6)
         if(!(n%i && n%(i+2))) return 0;
+    
     return true;
 }
