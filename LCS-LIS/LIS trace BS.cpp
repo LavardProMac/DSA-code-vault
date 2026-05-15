01112e87eb4de4d5ecf40ff9592f2d047c92fba6
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-15 22:37:48 +0700

    Create LIS trace BS.cpp

---
+++ LCS-LIS/LIS trace BS.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
+using namespace std;
+
+const int N=2e5+5;
+int a[N], p[N], id[N], t[N];
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m=1; cin>>n;
+    vector<int> v;
+    
+    fo(i,1,n){
+        cin>>a[i];
+        int k=lower_bound(p+1, p+m+1, a[i])-p;
+        
+        p[k]=a[i]; id[k]=i;
+        t[i]=id[k-1]; m=max(m, k);
+        
+    }
+    for(int i=id[m]; i; i=t[i])
+        v.push_back(a[i]);
+    reverse(v.begin(), v.end());
+    
+    cout<<v.size()<<'\n';
+    for(int i:v) cout<<i<<' ';
+}
