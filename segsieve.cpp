62c731503f31e13be6997f39390e2c1997e9f16c
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-16 00:57:27 +0700

    Update segsieve.cpp

--- segsieve.cpp
+++ segsieve.cpp
@@ -1,6 +1,9 @@
-inline void segSieve(const int& l, const int& r){
-    vector<bool> p(r-l+1, true); if(l==1) p[0]=false;
-    for(int i=2; i*i<=r; ++i)
-        for(int j=max(i*i, (l+i-1)/i*i); j<=r; j+=i) p[j-l]=false;
-    for(int i=0, k=r-l; i<=k; ++i) if(p[i]) cout<<i+l<<' ';
+void segmented_sieve(int l, int r){
+    bool p[r-l+1]={};
+    
+    for(ll i=2; i*i<=r; ++i)
+        for(ll j=max(i*i, (l+i-1)/i*i); j<=r; j+=i)
+            p[j-l]=1;
+
+    //fo(i,0,l-r) if(!p[i]) cout<<i+l<<' ';
 }
