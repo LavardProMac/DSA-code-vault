19679cade05f0b0fd8fed04a87c1eca5ba3909a2
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-17 23:42:20 +0700

    Update DP deque.cpp

--- Monotonic queue/DP deque.cpp
+++ Monotonic queue/DP deque.cpp
@@ -1,3 +1,5 @@
+// Đề: tìm max tổng giá trị khi chia mảng thành k đoạn con
+//     giá trị của 1 đoạn con = abs tổng đoạn đó
 // Source: oj.clue.edu.vn/problem/tn_hsg10_26_d
 
 #include<bits/stdc++.h>
@@ -16,20 +18,17 @@ int main(){
     
     fo(j,2,k){
         deque<int> d1, d2;
-        
         fo(i,1,n){
             const int t=i-1;
             ll t1=dp[t][j-1]+p[t], t2=dp[t][j-1]-p[t];
             
             while(!d1.empty() && dp[d1.back()][j-1]+p[d1.back()]<=t1)
                 d1.pop_back();
-            
             d1.push_back(t);
             if(d1.front()<=t-m) d1.pop_front();
             
             while(!d2.empty() && dp[d2.back()][j-1]-p[d2.back()]<=t2)
                 d2.pop_back();
-            
             d2.push_back(t);
             if(d2.front()<=t-m) d2.pop_front();
             
