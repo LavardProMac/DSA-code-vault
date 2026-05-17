d828e4df312482a1e41e8727eed7e396a9a78f07
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-17 16:28:30 +0700

    Update SQUARE.cpp

--- THI THỬ BỘ 2026/SQUARE.cpp
+++ THI THỬ BỘ 2026/SQUARE.cpp
@@ -4,58 +4,52 @@
 using namespace std;
 
 const int N=2005;
-int a[N][N], mx[N][N], mn[N][N];
+int a[N][N], mx[N][N], mi[N][N];
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int n, m, k, d; cin>>n>>m>>k>>d;
-    int s=k<<1|1, cnt=0;
+    int m, n, k, d; cin>>m>>n>>k>>d;
     
-    deque<int> q1, q2;
-    fo(i,1,n) fo(j,1,m) cin>>a[i][j];
-
-    fo(i,1,n){
-        q1.clear(); q2.clear();
-
-        fo(j,1,m){
-            while(!q1.empty() && a[i][q1.back()]<=a[i][j])
-                q1.pop_back();
-            q1.push_back(j);
-
-            while(q1.front()<=j-s) q1.pop_front();
-
-            while(!q2.empty() && a[i][q2.back()]>=a[i][j])
-                q2.pop_back();
-            q2.push_back(j);
-
-            while(q2.front()<=j-s) q2.pop_front();
-
-            if(j>=s)
-                mx[i][j]=a[i][q1.front()],
-                mn[i][j]=a[i][q2.front()];
+    int s=k<<1|1, cnt=0;
+    fo(i,1,m) fo(j,1,n) cin>>a[i][j];
+    
+    fo(j,1,m){
+        deque<int> d1, d2;
+        fo(i,1,n){
+            while(!d1.empty() && a[j][d1.back()]<=a[j][i])
+                d1.pop_back();
+                
+            d1.push_back(i);
+            if(d1.front()<=i-s) d1.pop_front();
+            
+            while(!d2.empty() && a[j][d2.back()]>=a[j][i])
+                d2.pop_back();
+                
+            d2.push_back(i);
+            if(d2.front()<=i-s) d2.pop_front();
+            
+            if(i>=s)
+                mx[j][i]=a[j][d1.front()],
+                mi[j][i]=a[j][d2.front()];
         }
     }
-    fo(j,s,m){
-        q1.clear(); q2.clear();
-
-        fo(i,1,n){
-            while(!q1.empty() && mx[q1.back()][j]<=mx[i][j])
-                q1.pop_back();
-            q1.push_back(i);
-
-            while(q1.front()<=i-s) q1.pop_front();
-
-            while(!q2.empty() && mn[q2.back()][j]>=mn[i][j])
-                q2.pop_back();
-            q2.push_back(i);
-
-            while(q2.front()<=i-s) q2.pop_front();
-
-            if(i>=s){
-                int ma=mx[q1.front()][j];
-                int mi=mn[q2.front()][j];
-                if(ma-mi>=d) ++cnt;
-            }
+    fo(j,s,n){
+        deque<int> d1, d2;
+        fo(i,1,m){
+            while(!d1.empty() && mx[d1.back()][j]<=mx[i][j])
+                d1.pop_back();
+                
+            d1.push_back(i);
+            if(d1.front()<=i-s) d1.pop_front();
+            
+            while(!d2.empty() && mi[d2.back()][j]>=mi[i][j])
+                d2.pop_back();
+                
+            d2.push_back(i);
+            if(d2.front()<=i-s) d2.pop_front();
+            
+            if(i>=s && mx[d1.front()][j]-mi[d2.front()][j]>=d)
+                ++cnt;
         }
     }
     cout<<cnt;
