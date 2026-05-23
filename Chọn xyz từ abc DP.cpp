fc45db1ac16f71e3b333d313f1a03348a45bafe1
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-23 15:42:18 +0700

    Implement DP solution for selecting xyz from abc

---
+++ Chọn xyz từ abc DP.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
+using namespace std;
+ll dp[105][105][105];
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int x, y, z; cin>>x>>y>>z;
+    int n=x+y+z;
+    
+    int a[n+1], b[n+1], c[n+1];
+    fo(i,1,n) cin>>a[i]>>b[i]>>c[i];
+    
+    memset(dp, -0x3f, sizeof dp);
+    dp[0][0][0]=0;
+
+    fo(i,0,n-1) fo(j,0,min(i, y)) fo(k,0,min(i-j, z)){
+        ll v=dp[i][j][k];
+        if(v<0) continue;
+        int t=i-j-k;
+
+        if(t<x)
+            dp[i+1][j][k]=max(dp[i+1][j][k], v+a[i+1]);
+        if(j<y)
+            dp[i+1][j+1][k]=max(dp[i+1][j+1][k], v+b[i+1]);
+        if(k<z)
+            dp[i+1][j][k+1]=max(dp[i+1][j][k+1], v+c[i+1]);
+    }
+    cout<<dp[n][y][z];
+}
