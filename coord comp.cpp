deea04a3c815464c2e5348d1023b82ffecacd892
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-23 23:28:02 +0700

    Update coord comp.cpp

--- coord comp.cpp
+++ coord comp.cpp
@@ -7,12 +7,13 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin>>n;
+    
     int a[n+1], b[n+1];
-
     fo(i,1,n) cin>>a[i], b[i]=a[i];
 
     sort(b+1, b+n+1);
     int m=unique(b+1, b+n+1)-b-1;
 
-    fo(i,1,n) a[i]=lower_bound(b+1, b+m+1, a[i])-b;
+    fo(i,1,n)
+        a[i]=lower_bound(b+1, b+m+1, a[i])-b;
 }
