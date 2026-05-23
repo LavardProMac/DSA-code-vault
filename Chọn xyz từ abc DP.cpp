f87d096d5eb4283df150e54aa002878694c745b2
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-23 15:52:54 +0700

    Update Chọn xyz từ abc DP.cpp

--- Chọn xyz từ abc DP.cpp
+++ Chọn xyz từ abc DP.cpp
@@ -1,8 +1,19 @@
+// Đề: Cho x + y + z = n và 3 mảng a[n], b[n], c[n]
+// Chọn x số từ a, y số từ b và z số từ c thỏa mãn:
+// - không có 2 số nào trùng chỉ số
+// - tổng tất cả các số được chọn là lớn nhất
+
 #include<bits/stdc++.h>
 #define ll long long
 #define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
+
 ll dp[105][105][105];
+// dp[i][j][k]: tổng lớn nhất khi xét tới vị trí i:
+// - đã chọn j số từ b
+// - đã chọn k số từ c
+// => số a đã chọn là: i−j−k
+// ĐPT: O(nyz)
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
@@ -19,12 +30,14 @@ int main(){
         ll v=dp[i][j][k];
         if(v<0) continue;
         int t=i-j-k;
-
-        if(t<x)
+        
+        if(t<x) // chọn ai
             dp[i+1][j][k]=max(dp[i+1][j][k], v+a[i+1]);
-        if(j<y)
+            
+        if(j<y) // chọn bi
             dp[i+1][j+1][k]=max(dp[i+1][j+1][k], v+b[i+1]);
-        if(k<z)
+            
+        if(k<z) // chọn ci
             dp[i+1][j][k+1]=max(dp[i+1][j][k+1], v+c[i+1]);
     }
     cout<<dp[n][y][z];
