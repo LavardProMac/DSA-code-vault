828a369c6316fcbcbba7de70b1f6ca613f1de633
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-05-26 15:18:25 +0700

    Update palin prep (DP).cpp

--- Palindrome query/palin prep (DP).cpp
+++ Palindrome query/palin prep (DP).cpp
@@ -8,11 +8,8 @@ string s; bool p[10001][10001];
 
 void prep(){
     int n=s.size(); s=' '+s;
+    fo(i,1,n) p[i][i]=1, p[i-1][i]=s[i-1]==s[i];
     
-    fo(i,1,n){
-        p[i][i]=1;
-        if(s[i]==s[i-1]) p[i-1][i]=1;
-    }
     for(int l=n-2; l; --l) fo(r,l+2,n)
         if(s[l]==s[r] && p[l+1][r-1]) p[l][r]=1;
 }
