4ebc15213c83085366262de6c65531ec7e7c7575
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-06-14 13:17:35 +0700

    Create DFS.cpp

---
+++ Graph/DFS.cpp
@@ -0,0 +1,26 @@
+// DFS O(N+M)
+
+#include<bits/stdc++.h>
+#define ll long long
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
+using namespace std;
+const int N=1e5+5;
+
+unordered_map<int, vector<int>> g;
+bitset<N> d;
+
+void dfs(int u){
+    cout<<u<<' '; d[u]=1;
+    for(int v:g[u]) if(!d[v]) dfs(v);
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m, u, v; cin>>n>>m;
+    
+    fo(i,1,m) cin>>u>>v,
+        g[u].push_back(v),
+        g[v].push_back(u);
+
+    fo(i,1,n) if(!d[i]) dfs(i);
+}
