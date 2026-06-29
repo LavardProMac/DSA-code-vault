dd7de72e2bc10d3c47d30c7ca8306f930e0b4545
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-06-29 23:01:08 +0700

    Update BFS.cpp

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -1,41 +1,54 @@
-// BFS O(N+M)
+// Source: cses.fi/problemset/task/1193
 
 #include<bits/stdc++.h>
-#define ll long long
-#define pb push_back
-#define all(a) a.begin(), a.end()
-#define fo(i,j,n) for(int i=j; i<=n; ++i)
+#define fo(i,l,r) for(int i=l; i<=r; i++)
 using namespace std;
-const int N=1e5+5;
+const int N=1001;
 
-unordered_map<int, vector<int>> g;
-unordered_map<int, int> d; // distance
-
-void bfs(int s){
-    queue<int> q;
-    q.push(s); d[s]=1;
-
-    while(!q.empty()){
-        int u=q.front(); q.pop();
-        // cout<<u<<' ';
-
-        for(int v:g[u]) if(!d[v])
-            d[v]=d[u]+1, q.push(v);
-    }
-}
+char a[N][N], tr[N][N]; bool vs[N][N];
+int dx[]={-1, 1, 0, 0};
+int dy[]={0, 0, -1, 1};
+char dc[]={'U', 'D', 'L', 'R'};
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int n, m, u, v; cin>>n>>m;
-    vector<int> a;
-    
-    fo(i,1,m) cin>>u>>v,
-        g[u].pb(v), g[v].pb(u),
-        a.pb(u), a.pb(v);
-        
-    sort(all(a));
-    a.erase(unique(all(a)), a.end());
+    int n, m, sx, sy, tx, ty;
+    cin>>n>>m; 
 
-    for(int i:a) if(!d[i]) bfs(i);
-    // for(int i:a) cout<<d[i]<<' ';
+    fo(i,1,n) fo(j,1,m){
+        cin>>a[i][j];
+        if(a[i][j]=='A') sx=i, sy=j;
+        else if(a[i][j]=='B') tx=i, ty=j;
+    }
+    queue<pair<int, int>> q;
+    vs[sx][sy]=1; q.push({sx, sy});
+
+    while(!q.empty()){
+        auto [x,y]=q.front(); q.pop();
+        fo(i,0,3){
+            int u=x+dx[i], v=y+dy[i];
+
+            if(u<1 || u>n || v<1 || v>m)
+                continue;
+            if(vs[u][v] || a[u][v]=='#')
+                continue;
+
+            vs[u][v]=1; tr[u][v]=dc[i];
+            q.push({u, v});
+        }
+    }
+    if(!vs[tx][ty]) return cout<<"NO", 0;
+    cout<<"YES\n"; string s;
+
+    while(tx!=sx || ty!=sy){
+        char c=tr[tx][ty];
+        s.push_back(c);
+
+        if(c=='U') ++tx;
+        else if(c=='D') --tx;
+        else if(c=='L') ++ty;
+        else --ty;
+    }
+    reverse(s.begin(), s.end());
+    cout<<s.size()<<'\n'<<s;
 }
