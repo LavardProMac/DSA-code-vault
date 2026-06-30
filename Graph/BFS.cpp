b07fcb8630ac08285393a6a3fa20fa2503976a39
LavardProMac <139695091+LavardProMac@users.noreply.github.com>
2026-06-30 21:13:00 +0700

    Update BFS.cpp

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -1,54 +1,39 @@
-// Source: cses.fi/problemset/task/1193
+// Source: cses.fi/problemset/task/1667
 
 #include<bits/stdc++.h>
-#define fo(i,l,r) for(int i=l; i<=r; i++)
+#define ll long long
+#define pb push_back
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
 using namespace std;
-const int N=1001;
 
-char a[N][N], tr[N][N]; bool vs[N][N];
-int dx[]={-1, 1, 0, 0};
-int dy[]={0, 0, -1, 1};
-char dc[]={'U', 'D', 'L', 'R'};
+const int N=1e5+5;
+vector<int> g[N]; int d[N], t[N];
+
+void bfs(){
+    memset(d, -1, sizeof d);
+    queue<int> q; q.push(1); d[1]=0;
+
+    while(!q.empty()){
+        int u=q.front(); q.pop();
+        for(int v:g[u]) if(d[v]==-1)
+            d[v]=d[u]+1, t[v]=u, q.push(v);
+    }
+}
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int n, m, sx, sy, tx, ty;
-    cin>>n>>m; 
+    int n, m, u, v; cin>>n>>m;
 
-    fo(i,1,n) fo(j,1,m){
-        cin>>a[i][j];
-        if(a[i][j]=='A') sx=i, sy=j;
-        else if(a[i][j]=='B') tx=i, ty=j;
-    }
-    queue<pair<int, int>> q;
-    vs[sx][sy]=1; q.push({sx, sy});
+    fo(i,1,m) cin>>u>>v,
+        g[u].pb(v), g[v].pb(u);
 
-    while(!q.empty()){
-        auto [x,y]=q.front(); q.pop();
-        fo(i,0,3){
-            int u=x+dx[i], v=y+dy[i];
+    bfs(); vector<int> a;
+    if(d[n]==-1)
+        return cout<<"IMPOSSIBLE", 0;
 
-            if(u<1 || u>n || v<1 || v>m)
-                continue;
-            if(vs[u][v] || a[u][v]=='#')
-                continue;
-
-            vs[u][v]=1; tr[u][v]=dc[i];
-            q.push({u, v});
-        }
-    }
-    if(!vs[tx][ty]) return cout<<"NO", 0;
-    cout<<"YES\n"; string s;
-
-    while(tx!=sx || ty!=sy){
-        char c=tr[tx][ty];
-        s.push_back(c);
-
-        if(c=='U') ++tx;
-        else if(c=='D') --tx;
-        else if(c=='L') ++ty;
-        else --ty;
-    }
-    reverse(s.begin(), s.end());
-    cout<<s.size()<<'\n'<<s;
+    for(int u=n; u; u=t[u]) a.pb(u);
+    reverse(a.begin(), a.end());
+    
+    cout<<a.size()<<'\n';
+    for(int i:a) cout<<i<<' ';
 }
