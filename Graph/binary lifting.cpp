c5e0e1e9bc4aa906e91b29e8c296bc5be08649d8
Nguyễn Viết Tiến <139695091+LavardProMac@users.noreply.github.com>
2026-07-04 17:17:51 +0700

    Create binary lifting.cpp

---
+++ Graph/binary lifting.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define fo(i,j,n) for(int i=j; i<=n; ++i)
+using namespace std;
+const int N=2e5+5, LOG=18;
+
+int d[N], up[N][LOG];
+vector<int> g[N];
+
+void dfs(int u, int p){
+    up[u][0]=p;
+    fo(i,1,LG) up[u][i]=
+        up[up[u][i-1]][i-1];
+
+    for(int v:g[u]) if(v!=p)
+        d[v]=d[u]+1, dfs(v, u);
+}
+
+int lca(int u, int v){
+    if(d[u]<d[v]) swap(u, v);
+
+    for(int i=LOG; i>=0; --i)
+        if(d[u]-(1<<i)>=d[v])
+            u=up[u][i];
+    if(u==v) return u;
+
+    for(int i=LOG; i>=0; --i)
+        if(up[u][i]!=up[v][i])
+            u=up[u][i], v=up[v][i];
+    return up[u][0];
+}
+
+inline int dis(int u, int v){
+    return d[u]+d[v]-2*d[lca(u, v)];
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, q, p; cin>>n;
+
+    fo(i,2,n) cin>>p,
+        g[p].push_back(i),
+        g[i].push_back(p);
+
+    dfs(1, 0); cin>>q;
+    while(q--) cin>>u>>v,
+        cout<<lca(u, v)<<'\n';
+}
