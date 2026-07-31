1a2bca36a760b844337899bed07aa1c2af0e837c
Nguyễn Viết Tiến <139695091+LavardProMac@users.noreply.github.com>
2026-07-31 21:11:01 +0700

    Update Trie 1.cpp

--- Graph/Trie 1.cpp
+++ Graph/Trie 1.cpp
@@ -1,38 +1,35 @@
-// Source: codeforces.com/gym/467680/problem/A
+// Source: www.spoj.com/problems/ADAINDEX
 
 #include<bits/stdc++.h>
 #define ll long long
 #define fo(i,j,n) for(int i=j; i<n; ++i)
 using namespace std;
-const int N=10005;
 
-int d[N*50][26], cnt;
-bitset<N*50> e;
+const int N=1e6+5;
+int d[N][26], cnt[N], sz;
 
 void add(const string& s){
     int u=0, n=s.size();
     fo(i,0,n){
         int k=s[i]-'a';
-        if(!d[u][k]) d[u][k]=++cnt;
-        u=d[u][k];
+        if(!d[u][k]) d[u][k]=++sz;
+        ++cnt[u=d[u][k]];
     }
-    e[u]=1;
 }
 
-bool qry(const string& s){
+int qry(const string& s){
     int u=0, n=s.size();
     fo(i,0,n){
         int k=s[i]-'a';
         if(!d[u][k]) return 0;
         u=d[u][k];
     }
-    return e[u];
+    return cnt[u];
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int n; cin>>n; string s;
+    int n, m; cin>>n>>m; string s;
     fo(i,0,n) cin>>s, add(s);
-    cin>>n;
-    while(cin>>s) cout<<qry(s)<<'\n';
+    fo(i,0,m) cin>>s, cout<<qry(s)<<'\n';
 }
