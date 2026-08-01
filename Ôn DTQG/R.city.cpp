ab69ed9eb7f4708f4b3b1278d0a16914680a8dc0
Nguyễn Viết Tiến <139695091+LavardProMac@users.noreply.github.com>
2026-08-01 10:14:32 +0700

    Update and rename city.cpp to R.city.cpp

--- Ôn DTQG/R.city.cpp
+++ Ôn DTQG/R.city.cpp
@@ -5,12 +5,9 @@
 #define pll pair<ll, ll>
 #define fo(i,l,r) for(int i=l; i<=r; ++i)
 using namespace std;
-const int N=1e5+5;
 
-vector<pll> e[N];
-vector<int> p; ll d[N];
-map<pll, pll> ban;
-vector<tuple<int, int, int>> ed;
+vector<pll> e[100005]; ll d[100005];
+map<pll, pll> ban; map<pll, int> len;
 priority_queue<pll, vector<pll>, greater<pll>> q;
 
 int main(){
@@ -18,24 +15,19 @@ int main(){
     int n, m, a, b, k, g;
     cin>>n>>m>>a>>b>>k>>g;
 
-    p.resize(g+1); map<pll, int> len;
+    int p[g+1], u, v, w; ll cur=-k;
     fo(i,1,g) cin>>p[i];
 
-    fo(i,1,m){
-        int u, v, w; cin>>u>>v>>w;
-        e[u].emplace_back(v, w);
-        e[v].emplace_back(u, w);
-        ed.emplace_back(u, v, w);
-    }
-    for(const auto& [u,v,w]:ed)
+    fo(i,1,m) cin>>u>>v>>w,
+        e[u].emplace_back(v, w),
+        e[v].emplace_back(u, w),
         len[{u, v}]=len[{v, u}]=w;
-
-    ll cur=-k;
+    
     fo(i,1,g-1){
         int u=p[i], v=p[i+1];
         ll w=len[{u, v}];
         ban[{u, v}]=ban[{v, u}]=
-        {cur, cur+w}; cur+=w;
+            {cur, cur+w}; cur+=w;
     }
     fo(i,1,n) d[i]=4e18;
     q.push({d[a]=0, a});
