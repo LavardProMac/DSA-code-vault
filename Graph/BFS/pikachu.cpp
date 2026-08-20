94bb134cdbca4be821fb8c076ebbf61e49e0266e
Nguyễn Viết Tiến <139695091+LavardProMac@users.noreply.github.com>
2026-08-20 14:52:49 +0700

    Update pikachu.cpp

--- Graph/BFS/pikachu.cpp
+++ Graph/BFS/pikachu.cpp
@@ -6,47 +6,50 @@
 using namespace std;
 const int N=105;
 
-char a[N][N];
-int d[N][N], vis[N][N], id;
+int m, n, ans; char a[N][N];
+int d[N][N], v[N][N], id;
+
 int dx[]={-1, 1, 0, 0};
 int dy[]={0,0, -1, 1};
 
-int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int m, n, ans=0; cin>>m>>n;
-    
-    fo(i,0,m+1) fo(j,0,n+1) a[i][j]='.';
-    fo(i,1,m) fo(j,1,n) cin>>a[i][j];
+void bfs(int i, int j){
+    queue<pair<int, int>> q;
+    q.push({i, j}); d[i][j]=0;
+    v[i][j]=++id;
 
-    fo(i,1,m) fo(j,1,n){
-        if(a[i][j]=='.') continue;
-        queue<pair<int, int>> q;
-        q.push({i, j}); d[i][j]=0;
-        vis[i][j]=++id;
+    while(!q.empty()){
+        auto [r, c]=q.front(); q.pop();
+        if(d[r][c]==3) continue;
 
-        while(!q.empty()){
-            auto [r, c]=q.front(); q.pop();
-            if(d[r][c]==3) continue;
-
-            fo(t,0,3){
-                int x=r+dx[t], y=c+dy[t];
-                while(x>=0 && x<=m+1 && y>=0 && y<=n+1){
-                    if(a[x][y]==a[i][j]){
-                        if((x>i || x==i && y>j) && vis[x][y]!=id)
-                            vis[x][y]=id, ++ans;
-                        break;
-                    }
-                    if(a[x][y]!='.') break;
-                    
-                    if(vis[x][y]!=id || d[x][y]>d[r][c]+1)
-                        vis[x][y]=id,
-                        d[x][y]=d[r][c]+1,
-                        q.push({x, y});
-                    
-                    x+=dx[t]; y+=dy[t];
+        fo(t,0,3){
+            int x=r+dx[t], y=c+dy[t];
+            while(x>=0 && x<=m+1 && y>=0 && y<=n+1){
+                if(a[x][y]==a[i][j]){
+                    if((x>i || x==i && y>j) && v[x][y]!=id)
+                        v[x][y]=id, ++ans;
+                    break;
                 }
+                if(a[x][y]!='.') break;
+
+                if(v[x][y]!=id || d[x][y]>d[r][c]+1)
+                    v[x][y]=id,
+                    d[x][y]=d[r][c]+1,
+                    q.push({x, y});
+
+                x+=dx[t]; y+=dy[t];
             }
         }
     }
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin>>m>>n;
+
+    fo(i,0,m+1) fo(j,0,n+1) a[i][j]='.';
+    fo(i,1,m) fo(j,1,n) cin>>a[i][j];
+
+    fo(i,1,m) fo(j,1,n)
+        if(a[i][j]!='.') bfs(i, j);
     cout<<ans;
 }
